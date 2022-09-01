25db00b677d02d59a9d4106010dc845e6c99b32d
cgsdfc <cgsdfc@126.com>
2022-09-01 14:19:22 +0800

    pyqt 已安装
    是否好使？

--- scripts/setup-python.cmd
+++ scripts/setup-python.cmd
@@ -1,1 +1,1 @@
-pip3 install -i http://mirrors.aliyun.com/simple  --trusted-host mirrors.aliyun.com  -r ./requirements.txt
+pip3 install -i https://mirrors.aliyun.com/pypi/simple/  --trusted-host mirrors.aliyun.com  -r ./requirements.txt
