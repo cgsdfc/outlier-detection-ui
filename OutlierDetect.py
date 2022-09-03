8d5da94abfaf1fcea28b1825a04c0ee836094535
cgsdfc <cgsdfc@126.com>
2022-09-03 11:05:23 +0800

    加载模型列表

--- OutlierDetect.py
+++ OutlierDetect.py
@@ -9,3 +9,41 @@
 2. 其他异常情况；
 3. 可视化的结果；
 """
+
+from pprint import pprint
+
+
+class Model:
+
+    RAW_TEXT = """\
+    #     from pyod.models.abod import ABOD
+    #     from pyod.models.auto_encoder import AutoEncoder
+    #     from pyod.models.cblof import CBLOF
+    #     from pyod.models.hbos import HBOS
+    #     from pyod.models.iforest import IForest
+    #     from pyod.models.knn import KNN
+    #     from pyod.models.lof import LOF
+    #     from pyod.models.mcd import MCD
+    #     from pyod.models.ocsvm import OCSVM
+    #     from pyod.models.pca import PCA\
+    """
+
+    @classmethod
+    def discover_models(self):
+        import re
+
+        PAT=re.compile(r'\s*\#\s*from pyod\.models\.(\w+) import (\w+)')
+        data=self.RAW_TEXT.splitlines()
+        data=list(map(lambda x: PAT.match(x).groups(), data))
+        return dict(data)
+
+
+if __name__=='__main__':
+    # myglobal={}
+    # exec('from pyod.models.auto_encoder import AutoEncoder', myglobal)
+    # pprint(myglobal)
+    # # pprint(vars(Model))
+
+
+
+    pprint(Model.discover_models())
--- README.md
+++ README.md
@@ -13,3 +13,37 @@ https://pyod.readthedocs.io/en/latest/example.html
 
 https://pyod.readthedocs.io/en/latest/pyod.utils.html#pyod.utils.data.generate_data
 
+所有模型
+
+https://pyod.readthedocs.io/en/latest/pyod.models.html
+
+模型列表
+```
+-*- coding: utf-8 -*-
+from .abod import ABOD
+from .auto_encoder import AutoEncoder
+from .cblof import CBLOF
+from .combination import aom, moa, average, maximization
+from .feature_bagging import FeatureBagging
+from .hbos import HBOS
+from .iforest import IForest
+from .knn import KNN
+from .lof import LOF
+from .mcd import MCD
+from .ocsvm import OCSVM
+from .pca import PCA
+
+__all__ = ['ABOD',
+           'AutoEncoder',
+           'CBLOF',
+           'aom', 'moa', 'average', 'maximization',
+           'FeatureBagging',
+           'HBOS',
+           'IForest',
+           'KNN',
+           'LOF',
+           'MCD',
+           'OCSVM',
+           'PCA']
+
+```
