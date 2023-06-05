70bbc945033ca53d7da032a6929d76127ee469e3
cgsdfc <cgsdfc@126.com>
2023-06-05 19:25:35 +0800

    开始调试前后端

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+function Parameters() {
+    this.select_model = document.getElementById("select-model").value;
+    this.training_set = document.getElementById("training-set").value;
+    this.testing_set = document.getElementById("testing-set").value;
+    this.outlier_ratio = document.getElementById("outlier-ratio").value;
+    this.feature_dims = document.getElementById("feature-dims").value;
+    this.random_seed = document.getElementById("random-seed").value;
+
+    console.log('Load parameters');
+    for (let key in this) {
+        console.log(`${key} => ${this[key]}`);
+    }
+    this.as_url = function () {
+        var out = [];
+        for (let key in this) {
+            out.push(`${key}=${this[key]}`);
+        }
+        return out.join('&');
+    }
+}
+
+var parameters;
+
+function updateParams() {
+    parameters = new Parameters();
+}
+
+window.onload(() => { updateParams(); });
+
+const URL = 'http://127.0.0.1:5000';
+
 function run() {
-    
+    updateParams();
+    const param_str = parameters.as_url();
+    fetch(`${URL}/run?${param_str}`)
+        .then((data) => data.json())
+        .then((data) => {
+            var imgdata = data['image'];
+            imgdata = `data:image/png;base64,${imgdata}`;
+            const canvas = document.getElementById("image-canvas");
+            canvas.innerHTML = `<img src=${imgdata}/>`;
+            console.log(`Get run ${data}`);
+        });
 }
--- server.py
+++ server.py
@@ -1,8 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask_cors import CORS
+from src.OutlierDetect import DetectionEvaluator, DataConfig, ModelConfig
+import logging
+import base64
 
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 CORS(app)
+
+PARAMS = dict(
+    select_model=str,
+    training_set=int,
+    testing_set=int,
+    outlier_ratio=float,
+    feature_dims=int,
+    random_seed=int,
+)
+
+logging.info(PARAMS)
+
+
+def parse_params():
+    out = {}
+    for name, type in PARAMS.items():
+        val = request.args[name]
+        val = type(val)
+        out[name] = val
+    return out
 
 
 @app.route("/")
@@ -10,10 +34,33 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/run", methods=["POST"])
-def calculate():
-    expression = request.args["expression"]
+@app.route("/run", methods=["GET"])
+def run():
+    params = parse_params()
+    ev = DetectionEvaluator()
+    cfg = DataConfig(
+        n_train=params["training_set"],
+        n_test=params["testing_set"],
+        n_features=params["feature_dims"],
+        seed=params["random_seed"],
+        contamination=params["outlier_ratio"],
+    )
+    logging.info("Load data begins")
+    ev.load_data(config=cfg)
 
+    logging.info("Load model begins")
+    cfg = ModelConfig(name=params["select_model"])
+    ev.load_model(config=cfg)
+
+    logging.info("Detect begins")
+    ev.detect()
+
+    logging.info("Visualize begins")
+    image = ev.visualize()
+    image = base64.b64encode(image.read_bytes())
+    logging.info(f"Base64 {image}")
+
+    return make_response(image)
 
 
 if __name__ == "__main__":
