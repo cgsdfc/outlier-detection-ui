ef0d5f03a1095c2758952dd96fa0419d074a2dfd
fengcong <cgsdfc@126.com>
2023-06-11 15:56:27 +0800

    迁移了基于Vite和setup的写法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,52 +4,54 @@
     <div id="parameters" class="frame">
       <div class="param">
         <label for="select-model">Select a Model</label>
-        <select v-model="select_model" id="select-model">
-          <option v-for="m in methods" :key="m" :value="m" :label="m"></option>
+        <select v-model="state.select_model" id="select-model">
+          <option v-for="m in METHODS" :key="m" :value="m" :label="m"></option>
         </select>
       </div>
       <div class="param">
         <label for="training-set">#Training Set</label>
-        <input type="number" id="training-set" v-model="training_set">
+        <input type="number" id="training-set" v-model="state.training_set">
       </div>
 
       <div class="param">
         <label for="testing-set">#Testing Set</label>
-        <input type="number" id="testing-set" v-model="testing_set">
+        <input type="number" id="testing-set" v-model="state.testing_set">
       </div>
 
       <div class="param">
         <label for="outlier-ratio">Outliers%</label>
-        <input type="number" id="outlier-ratio" v-model="outlier_ratio" step=".01" min="0" max="1">
+        <input type="number" id="outlier-ratio" v-model="state.outlier_ratio" step=".01" min="0" max="1">
       </div>
       <div class="param">
         <label for="feature-dims">Feature Dims</label>
-        <input type="number" id="feature-dims" v-model="feature_dims">
+        <input type="number" id="feature-dims" v-model="state.feature_dims">
       </div>
       <div class="param">
         <label for="random-seed">Random Seed</label>
-        <input type="number" id="random-seed" v-model="random_seed">
+        <input type="number" id="random-seed" v-model="state.random_seed">
       </div>
     </div>
 
     <div id="image-canvas" class="frame">
       <!-- Add JavaScript to dynamically generate the image and display it in this canvas -->
-      <img :src="result_image ? result_image : default_image" alt="Detection Result" />
+      <img :src="display_image" alt="Detection Result" />
     </div>
 
     <div id="status-bar" class="frame">
-      <span id="status-label" class="frame">{{ status }}</span>
-      <progress :value="pgb" max="100" id="progress-bar"></progress>
+      <span id="status-label" class="frame">{{ state.status }}</span>
+      <progress :value="state.pgb" max="100" id="progress-bar"></progress>
       <button id="run-btn" class="frame" @click="run()">RUN</button>
     </div>
 
   </div>
 </template>
 
-<script>
-// import '@/assets/splash.png'
+<script setup>
+import splash from '@/assets/splash.png'
+import { computed } from '@vue/reactivity';
+import { reactive } from '@vue/reactivity';
 
-export const PARAMS = [
+const PARAMS = [
   'select_model',
   'training_set',
   'testing_set',
@@ -57,76 +59,76 @@ export const PARAMS = [
   'feature_dims',
   'random_seed',
 ];
-export const URL = '/api';
 
-export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-      select_model: 'KNN',
-      training_set: 1000,
-      testing_set: 100,
-      outlier_ratio: 0.1,
-      feature_dims: 10,
-      random_seed: 42,
-      status: 'Ready',
-      pgb: 0,
-      result_image: null,
-      default_image: require('@/assets/splash.png'),
-      methods: [
-        "ABOD",
-        "HBOS",
-        "IForest",
-        "KNN",
-        "LOF",
-        "MCD",
-        "OCSVM",
-        "PCA",
-      ]
-    }
-  },
-  methods: {
-    get_params() {
-      console.log('Load parameters');
-      var out = [];
-      for (let key of PARAMS) {
-        console.log(`${key} => ${this[key]}`);
-        out.push(`${key}=${this[key]}`);
-      }
-      return out.join('&');
-    },
+const URL = '/api';
 
-    run() {
-      const param_str = this.get_params();
-      this.pgb = 0;
-      this.status = 'Ready';
+const METHODS = [
+  "ABOD",
+  "HBOS",
+  "IForest",
+  "KNN",
+  "LOF",
+  "MCD",
+  "OCSVM",
+  "PCA",
+]
 
-      fetch(`${URL}/load_data?${param_str}`)
-        .then(() => {
-          this.pgb = 25;
-          this.status = 'Data Loaded';
-          return fetch(`${URL}/load_model?${param_str}`);
-        })
-        .then(() => {
-          this.pgb = 50;
-          this.status = 'Model Loaded';
-          return fetch(`${URL}/detect?${param_str}`);
-        })
-        .then(() => {
-          this.pgb = 75;
-          this.status = 'Detected';
-          return fetch(`${URL}/visualize?${param_str}`);
-        })
-        .then((data) => data.json())
-        .then((data) => {
-          var imgdata = data['image'];
-          imgdata = `data:image/png;base64,${imgdata}`;
-          this.result_image = imgdata;
-          this.pgb = 100;
-          this.status = 'Done';
-        });
-    }
+const state = reactive({
+  select_model: 'KNN',
+  training_set: 1000,
+  testing_set: 100,
+  outlier_ratio: 0.1,
+  feature_dims: 10,
+  random_seed: 42,
+  status: 'Ready',
+  pgb: 0,
+  result_image: null,
+  default_image: splash,
+})
+
+const display_image = computed(() => state.result_image ?
+  state.result_image : state.default_image)
+
+function get_params() {
+  console.log('Load parameters');
+  var out = [];
+  for (let key of PARAMS) {
+    console.log(`${key} => ${state[key]}`);
+    out.push(`${key}=${state[key]}`);
   }
+  return out.join('&');
+}
+
+function run() {
+  const param_str = get_params();
+  console.log(`params_str ${param_str}`)
+  state.pgb = 0;
+  state.status = 'Ready';
+
+  fetch(`${URL}/load_data?${param_str}`)
+    .then(() => {
+      state.pgb = 25;
+      state.status = 'Data Loaded';
+      return fetch(`${URL}/load_model?${param_str}`);
+    })
+    .then(() => {
+      state.pgb = 50;
+      state.status = 'Model Loaded';
+      return fetch(`${URL}/detect?${param_str}`);
+    })
+    .then(() => {
+      state.pgb = 75;
+      state.status = 'Detected';
+      return fetch(`${URL}/visualize?${param_str}`);
+    })
+    .then((data) => data.json())
+    .then((data) => {
+      var imgdata = data['image'];
+      imgdata = `data:image/png;base64,${imgdata}`;
+      state.result_image = imgdata;
+      state.pgb = 100;
+      state.status = 'Done';
+    });
 }
 
 </script>
